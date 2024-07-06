5cb321d4c121d8c46cca6dc554acb06e0cd12e96
Alex Aichinger <alex.aichinger@me.com>
2024-07-07 00:43:01 +0100

    initial microservice setup

---
+++ src/main/kotlin/com/alexaichinger/nutri_tracking/NutriTrackingApplication.kt
@@ -0,0 +1,11 @@
+package com.alexaichinger.nutri_tracking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NutriTrackingApplication
+
+fun main(args: Array<String>) {
+	runApplication<NutriTrackingApplication>(*args)
+}
---
+++ src/test/kotlin/com/alexaichinger/nutri_tracking/NutriTrackingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alexaichinger.nutri_tracking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NutriTrackingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
