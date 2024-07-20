f8368ddf9b3bcbec92e6db2419067fa1084b30de
alexaichinger <alex.aichinger@me.com>
2024-07-20 16:04:55 +0100

    - tweaked some things to publish image

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,17 @@ plugins {
     id("io.spring.dependency-management") version "1.1.6"
 
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    id("com.palantir.git-version") version "3.1.0"
 
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.spring") version "2.0.0"
 }
 
+val gitVersion: groovy.lang.Closure<String> by extra
+version = gitVersion()
+
 group = "com.alexaichinger"
-version = "VERSION_PLACEHOLDER"
+version = "$version"
 
 java {
     toolchain {
