6167f92dc084e30d3480720d335250a2b27fdb21
alexaichinger <alex.aichinger@me.com>
2024-07-30 13:47:10 +0100

    - refactored products endpoint

--- src/main/kotlin/com/alexaichinger/nutritracking/web/ProductInformationController.kt
+++ src/main/kotlin/com/alexaichinger/nutritracking/web/ProductInformationController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 class ProductInformationController(
     private val foodFactsService: OpenFoodFactsService,
 ) {
